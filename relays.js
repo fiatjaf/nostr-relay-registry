42b75af84dd85d7afc70c96085e6fff08ae21f0f
七海灯 <uso800@h3z.jp>
2023-02-06 16:30:59 +0900

    Add Relay Server

--- relays.js
+++ relays.js
@@ -66,7 +66,8 @@ export const relays = [
   'wss://relay.nostr-latam.link',
   'wss://nostr.coinsamba.com.br',
   'wss://deconomy-netser.ddns.net:2121',
-  'wss://relay.nostr.wf'
+  'wss://relay.nostr.wf',
+  'wss://nostr.h3z.jp'
 ]
 
 shuffle(relays)
